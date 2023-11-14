6135fae9b61250161f92aa4891fd1f07546d788d
daar1707 <daar1707@gmail.com>
2023-11-14 20:01:31 +0100

    Region updated

--- lib/s3client.ts
+++ lib/s3client.ts
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
-  region: "eu-north-1",
+  region: "eu-central-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
