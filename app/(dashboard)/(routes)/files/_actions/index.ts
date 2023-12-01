c9aa8f5f8604221b7273c980585c8ae631140ed1
zainraza2000 <zainraza20000@gmail.com>
2023-12-01 22:09:25 +0500

    implemented status for file upload

--- app/(dashboard)/(routes)/files/_actions/index.ts
+++ app/(dashboard)/(routes)/files/_actions/index.ts
@@ -1,20 +1,26 @@
-"use server"
-import { DeleteObjectCommand, HeadObjectCommand, ListObjectsV2Command, PutObjectCommand, _Object } from "@aws-sdk/client-s3"
-import client from "@/lib/s3client"
-import { extractRightOfSlash } from "@/lib/utils"
-import { revalidatePath } from "next/cache"
+"use server";
+import {
+  DeleteObjectCommand,
+  HeadObjectCommand,
+  ListObjectsV2Command,
+  PutObjectCommand,
+  _Object,
+} from "@aws-sdk/client-s3";
+import client from "@/lib/s3client";
+import { extractRightOfSlash } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
 
-const BUCKET_NAME = "lovagent-files"
+const BUCKET_NAME = "lovagent-files";
 
 /**
  * This whole thing will be rewritten in python so don't mind.
  */
 
 export type S3Response = Omit<_Object, "StorageClass"> & {
-  StorageClass?: string
-  OriginalKey?: string
-  StatusProps?: Record<string, string>
-}
+  StorageClass?: string;
+  OriginalKey?: string;
+  StatusProps?: Record<string, string>;
+};
 
 export async function getFoldersAndFiles(prefix: string) {
   const files = await client.send(
@@ -22,40 +28,57 @@ export async function getFoldersAndFiles(prefix: string) {
       Bucket: BUCKET_NAME,
       Prefix: `${prefix}/`,
     })
-  )
+  );
 
-  let allObjects: S3Response[] = []
+  let allObjects: S3Response[] = [];
   if (files.Contents) {
     allObjects = [
-      ...files.Contents.filter(content => {
-        if (content.Key?.endsWith("/") && content.Key === `${prefix}/${extractRightOfSlash(content.Key!.slice(0, -1))}/`) return true
-        else if (!content.Key?.endsWith("/") && content.Key === `${prefix}/${extractRightOfSlash(content.Key!)}`) return true
-        else return false
-      })
+      ...files.Contents.filter((content) => {
+        if (
+          content.Key?.endsWith("/") &&
+          content.Key ===
+            `${prefix}/${extractRightOfSlash(content.Key!.slice(0, -1))}/`
+        )
+          return true;
+        else if (
+          !content.Key?.endsWith("/") &&
+          content.Key === `${prefix}/${extractRightOfSlash(content.Key!)}`
+        )
+          return true;
+        else return false;
+      }),
     ];
-    allObjects = await Promise.all(allObjects.map(async (content) => {
-      if (content.Key?.endsWith("/")) return {
-        ...content,
-        Key: extractRightOfSlash(content.Key!.slice(0, -1)),
-        StorageClass: "DIRECTORY",
-        OriginalKey: content.Key
-      }
-      else {
-        const statusReq = await fetch(`http://localhost:8000/file_status?s3_key=${content.Key}`)
-        const statusProps: Record<string, string> = await statusReq.json()
-        return {
-          ...content,
-          Key: extractRightOfSlash(content.Key!),
-          OriginalKey: content.Key,
-          StatusProps: statusProps
+    allObjects = await Promise.all(
+      allObjects.map(async (content) => {
+        if (content.Key?.endsWith("/"))
+          return {
+            ...content,
+            Key: extractRightOfSlash(content.Key!.slice(0, -1)),
+            StorageClass: "DIRECTORY",
+            OriginalKey: content.Key,
+          };
+        else {
+          const statusReq = await fetch(
+            `http://localhost:8000/file_status?s3_key=${content.Key}`
+          );
+          const statusProps: Record<string, string> = await statusReq.json();
+          return {
+            ...content,
+            Key: extractRightOfSlash(content.Key!),
+            OriginalKey: content.Key,
+            StatusProps: statusProps,
+          };
         }
-      }
-    }))
+      })
+    );
   }
   return allObjects.sort((a, b) => {
-    if (a.StorageClass === 'DIRECTORY' && b.StorageClass !== 'DIRECTORY') {
+    if (a.StorageClass === "DIRECTORY" && b.StorageClass !== "DIRECTORY") {
       return -1;
-    } else if (a.StorageClass !== 'DIRECTORY' && b.StorageClass === 'DIRECTORY') {
+    } else if (
+      a.StorageClass !== "DIRECTORY" &&
+      b.StorageClass === "DIRECTORY"
+    ) {
       return 1;
     } else {
       return 0;
@@ -63,82 +86,100 @@ export async function getFoldersAndFiles(prefix: string) {
   });
 }
 
-export async function deleteObject(key: string, path?: string, revalidate = true) {
-  const file = await client.send(new HeadObjectCommand({
-    Bucket: BUCKET_NAME,
-    Key: key,
-  }))
-  const formData = new FormData()
-  formData.set("key", key)
+export async function deleteObject(
+  key: string,
+  path?: string,
+  revalidate = true
+) {
+  const file = await client.send(
+    new HeadObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: key,
+    })
+  );
+  const formData = new FormData();
+  formData.set("key", key);
   const response = await fetch(
     `http://localhost:8000/delete_doc/${file.Metadata!["meili-doc-id"]}`,
     {
       method: "POST",
-      body: formData
+      body: formData,
     }
-  )
+  );
   if (response.ok) {
-    const data = await response.json()
+    const data = await response.json();
     if (data.status === "success") {
-      if (revalidate && path) revalidatePath("/internal")
+      if (revalidate && path) revalidatePath("/internal");
     } else {
-      console.error(data.message)
+      console.error(data.message);
     }
   } else {
-    console.error("Error:", await response.text())
+    console.error("Error:", await response.text());
   }
 }
 
 export async function createFolder(folderPath: string, appPath: string) {
   const command = new PutObjectCommand({
     Bucket: BUCKET_NAME,
-    Key: `${folderPath}/`
-  })
-  await client.send(command)
-  revalidatePath(appPath)
+    Key: `${folderPath}/`,
+  });
+  await client.send(command);
+  revalidatePath(appPath);
 }
 
-export async function deleteFolder(key: string, path?: string, revalidate = true) {
-  const files = await client.send(new ListObjectsV2Command({
-    Bucket: BUCKET_NAME,
-    Prefix: key
-  }))
-  const objectKeys = [
-    ...files.Contents?.map(obj => obj.Key) ?? [],
-  ];
+export async function deleteFolder(
+  key: string,
+  path?: string,
+  revalidate = true
+) {
+  const files = await client.send(
+    new ListObjectsV2Command({
+      Bucket: BUCKET_NAME,
+      Prefix: key,
+    })
+  );
+  const objectKeys = [...(files.Contents?.map((obj) => obj.Key) ?? [])];
   await Promise.all(
     objectKeys.map(async (key) => {
-      const res = await client.send(new HeadObjectCommand({
-        Bucket: BUCKET_NAME,
-        Key: key
-      }))
+      const res = await client.send(
+        new HeadObjectCommand({
+          Bucket: BUCKET_NAME,
+          Key: key,
+        })
+      );
       if (res.Metadata?.["meili-doc-id"]) {
-        const formData = new FormData()
-        formData.set("key", key!)
+        const formData = new FormData();
+        formData.set("key", key!);
         await fetch(
           `http://localhost:8000/delete_doc/${res.Metadata["meili-doc-id"]}`,
           {
             method: "POST",
-            body: formData
+            body: formData,
           }
-        )
+        );
       } else {
-        client.send(new DeleteObjectCommand({
-          Bucket: BUCKET_NAME,
-          Key: key
-        }))
+        client.send(
+          new DeleteObjectCommand({
+            Bucket: BUCKET_NAME,
+            Key: key,
+          })
+        );
       }
     })
-  )
-  if (revalidate && path) revalidatePath(path)
+  );
+  if (revalidate && path) revalidatePath(path);
 }
 
-export default async function bulkDelete(objects: Pick<S3Response, "OriginalKey" | "StorageClass">[], path: string) {
+export default async function bulkDelete(
+  objects: Pick<S3Response, "OriginalKey" | "StorageClass">[],
+  path: string
+) {
   await Promise.all(
-    objects.map(object => {
-      if (object.StorageClass === "DIRECTORY") deleteFolder(object.OriginalKey!, "", false)
-      else deleteObject(object.OriginalKey!, "", false)
+    objects.map((object) => {
+      if (object.StorageClass === "DIRECTORY")
+        deleteFolder(object.OriginalKey!, "", false);
+      else deleteObject(object.OriginalKey!, "", false);
     })
-  )
-  revalidatePath(path)
+  );
+  revalidatePath(path);
 }
--- components/upload-files.tsx
+++ components/upload-files.tsx
@@ -1,21 +1,40 @@
-"use client"
-import { refreshPage } from "@/lib/util-actions"
-import { useSession } from "next-auth/react"
-import { Upload } from "lucide-react"
-import { usePathname } from "next/navigation"
-import { useRef, useTransition } from "react"
+"use client";
+import { refreshPage } from "@/lib/util-actions";
+import { useSession } from "next-auth/react";
+import { Upload } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useRef, useState, useTransition } from "react";
 
 const UploadButton = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const pathname = usePathname()
-  const [isPending, startTransition] = useTransition()
-  const { data: session } = useSession()
+  const inputRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
+  const { data: session } = useSession();
+  const [onChange, setOnChange] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    let sse: EventSource;
+    if (onChange) {
+      sse = new EventSource("http://localhost:8000/api/convert_document/sse");
+
+      function handleStream(e: MessageEvent<any>) {
+        router.refresh();
+      }
+
+      sse.onmessage = (e) => handleStream(e);
+      sse.onerror = (e) => sse.close();
+    }
+    return () => {
+      sse?.close();
+    };
+  }, [onChange]);
 
   return (
     <>
       <button
         onClick={() => {
-          inputRef.current?.click()
+          inputRef.current?.click();
         }}
         className="bg-transparent font-medium text-blue-700 hover:bg-neutral-100 rounded-none px-10 py-5 flex items-center"
       >
@@ -38,29 +57,30 @@ const UploadButton = () => {
         multiple
         onChange={(e) => {
           if (e.target.files?.length) {
-            const formData = new FormData()
+            setOnChange(true);
+            const formData = new FormData();
             for (const file of e.target.files) {
-              formData.append("files", file)
+              formData.append("files", file);
             }
             // formData.set("file", e.target.files[0])
-            formData.set("email", session?.user?.email ?? "")
+            formData.set("email", session?.user?.email ?? "");
             formData.set(
               "file_path",
               pathname.replace("/files", session?.user?.email ?? "")
-            )
+            );
             startTransition(async () => {
               const res = await fetch("/api/uploadpdf", {
                 method: "POST",
                 body: formData,
-              })
-              const resp = await res.json()
-              if (resp.status === "success") await refreshPage(pathname)
-            })
+              });
+              const resp = await res.json();
+              if (resp.status === "success") await refreshPage(pathname);
+            });
           }
         }}
       />
     </>
-  )
-}
+  );
+};
 
-export default UploadButton
+export default UploadButton;
